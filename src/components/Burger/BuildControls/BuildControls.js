55394cb1fae8da882200553f2433829628ff2a4c
William Lopera <lopera.william@gmail.com>
2020-03-22 12:44:34 -0500

    Cambiar  a español  ingredientes

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -4,10 +4,10 @@ import classes from "./BuildControls.css";
 import BuildControl from "./BuildControl/BuildControl";
 
 const controls = [
-  { label: "Salad", type: "salad" },
-  { label: "Bacon", type: "bacon" },
-  { label: "Cheese", type: "cheese" },
-  { label: "Meat", type: "meat" }
+  { label: "Ensalada", type: "salad" },
+  { label: "Tocineta", type: "bacon" },
+  { label: "Queso", type: "cheese" },
+  { label: "Carne", type: "meat" }
 ];
 
 const buildControls = (props) => (
